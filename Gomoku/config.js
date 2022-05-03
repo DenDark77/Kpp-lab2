562e774d455da531606d29ac435f2c13e6249ef6
DenDark77 <alekseystepka5589@gmail.com>
2022-05-03 19:12:44 +0300

    Kpp-lab2

---
+++ Gomoku/config.js
@@ -0,0 +1,13 @@
+var Config = {
+    WinStreak: 5,
+    Wide: 20,
+    Tall: 20,
+    BoxOffset: {
+        x: 1,
+        y: 1
+    },
+    Playername1: "Player 1",
+    Playername2: "Player 2"
+}
+
+module.exports = Config
---
+++ Gomoku/src/player.js
@@ -0,0 +1,19 @@
+var blessed = require('blessed');
+var program = blessed.program();
+var Config = require("../config")
+
+class Player {
+    x = 0;
+    y = 0;
+    Name = "Unnamed";
+
+    constructor(Name, Char) {
+        this.Name = Name;
+        this.Char = Char;
+    }
+    focus() {
+        program.move(this.x + Config.BoxOffset.x, this.y + Config.BoxOffset.y);
+    }
+}
+
+module.exports = Player;
